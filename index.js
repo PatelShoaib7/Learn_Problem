7c23b97e82079a450f7fa09065ee72d55e2278ef
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2023-02-08 12:32:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function GET_HINT_OF_OP(secret, guess) {
+    let cows = 0;
+    let bulls = 0;
+    const occurrences = Array(10).fill(0);
+
+    for (let i = 0; i < secret.length; i++) {
+        const guessNumber = parseInt(guess[i]);
+        const secretNumber = parseInt(secret[i]);
+        
+        if (secretNumber === guessNumber) {
+            bulls++;
+            continue
+        }
+
+        if(occurrences[secretNumber] < 0) {
+            cows++;
+        }
+        
+        if(occurrences[guessNumber] > 0) {
+            cows++;
+        }
+        
+        occurrences[secretNumber]++;
+        occurrences[guessNumber]--;
+    }
+  
+    return `${bulls}A ${cows}B`;
+}
+console.log(GET_HINT_OF_OP(4271 ,1))
